2bc5ed8e1abb7ce6d35f1fe6fb7b598ced4bad20
Roie Drukman <roie.drukman@post.runi.ac.il>
2024-11-07 18:34:36 +0200

    Update FVCalc.java

--- FVCalc.java
+++ FVCalc.java
@@ -2,9 +2,10 @@
 public class FVCalc {
 	public static void main(String[] args){
 		int currentValue = Integer.parseInt(args[0]); //current value
-		double rate = Double.parseDouble(args[1]); //rate - gets string and parse to double
+		double rate = (Double.parseDouble(args[1]));
+		double rate2 = ((100+rate)/100);
 		int years = Integer.parseInt(args[2]); //years - gets string and parse to int
-		double futureValue = (currentValue*(Math.pow((1+rate), years)));
-		System.out.println("After " + years + " years, a $" + currentValue + " saved at " + rate + "% will yield $" + ((int)futureValue)/100);
+		double futureValue = currentValue*Math.pow(rate2, years);
+		System.out.println("After " + years + " years, a $" + currentValue + " saved at " + rate + "% will yield $" + ((int)futureValue));
 	}
 }
